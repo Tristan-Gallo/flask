2f0801f2e1f5b9e4d445158d352fc5e714b772c0
Tristan-Gallo <gallo.tristan@iisgalvanimi.edu.it>
2022-02-23 13:53:49 +0000

    ho messo immagini però meglio

--- esercizio1.py
+++ esercizio1.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def immagini():
+def meteo():
     return render_template('index2.html')
 
 if __name__ == '__main__':
